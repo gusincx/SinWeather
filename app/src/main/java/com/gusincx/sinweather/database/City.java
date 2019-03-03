efc692dee3d135a2c66d0b4d604bb82032d616ec
gusin <gusionpkc@gmail.com>
2019-03-03 18:01:09 +0800

    加入数据库和标的各项配置

---
+++ app/src/main/java/com/gusincx/sinweather/database/City.java
@@ -0,0 +1,41 @@
+package com.gusincx.sinweather.database;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+    public int getProvinceID(){
+        return provinceid;
+    }
+
+    public void setProvinceid(int provinceid) {
+        this.provinceid = provinceid;
+    }
+}
